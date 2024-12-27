9558139f7daf6e6593452f4a420efe6f8c92d9b5
liarles <62631264+liarles@users.noreply.github.com>
2024-12-27 15:53:27 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,26 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum = 0, sum2 = 0, SD =0, GM=1, HM=0;
+    
+    double Max = A[0], Min = A[0];
+    for(int i = 0 ; i < N ; i++){
+        sum += A[i];
+        sum2 += A[i]*A[i];
+        
+        GM *=A[i];
+        HM += 1/A[i];
+        if (Max < A[i]) Max = A[i];
+        if (Min > A[i]) Min = A[i];
+    }
+    
+    
+    B[0] = sum/N;
+    SD = (sum2/N) - B[0]*B[0];
+    B[1] = sqrt(SD);
+    B[2] = pow(GM,1.0/N);
+    B[3] = N/HM;
+    B[4] = Max;
+    B[5] =Min;
+}
